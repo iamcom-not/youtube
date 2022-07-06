c48c2445d0345911294c4b45bef06734b4dc9899
iamcom-not <71725196+iamcom-not@users.noreply.github.com>
2022-07-07 05:42:56 +0900

    more pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,6 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => {
-    return res.send(`See Video #${req.params.id}`);
-}
-export const edit = (req, res) => {
-    return res.send(`Edit Video #${req.params.id}`);
-}
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const search =(req, res) => res.send("Search");
 export const upload =(req, res) => res.send("upload");
 export const deleteVideo =(req, res) => {
