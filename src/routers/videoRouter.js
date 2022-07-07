1841cc54475d5937f865a64da99352f88defc36b
iamcom-not <71725196+iamcom-not@users.noreply.github.com>
2022-07-08 02:14:43 +0900

    #6.6 finished fake db

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import { watch, getEdit,postEdit, upload, deleteVideo, getUpload, postUpload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 // 정규식 같은거 사용해서 숫자만 오게 해서 upload가 맨뒤에 와도 작동됨
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
+// videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+// videoRouter.get("/upload", upload);
 
 
 export default videoRouter;
